f80ce26539fc6a2d9ce4800b88304a56d38b87f4
Luuk Lenders <luuklsll@gmail.com>
2019-06-06 13:46:36 +0200

    Pov-changer
    Having some issues with git. Trying to fix them

---
+++ Snap To Grid/Assets/Scripts/ChangePOV.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePOV : MonoBehaviour
+{
+
+    public Transform Player;
+    public Camera FirstPersonCam, ThirdPersonCam;
+    public KeyCode TKey;
+    public GameObject FirstpersonControls;
+    public bool camSwitch = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(TKey))
+        {
+            camSwitch = !camSwitch;
+            FirstPersonCam.gameObject.SetActive(camSwitch);
+            ThirdPersonCam.gameObject.SetActive(!camSwitch);
+            FirstpersonControls.SetActive(camSwitch);
+            if (!camSwitch)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else if(camSwitch)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+}
---
+++ Snap To Grid/Assets/Scripts/CubePlacer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class CubePlacer : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+
+        else if (Input.GetMouseButton(1))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                RemoveCubeNear(hitInfo.point);
+                if (hitInfo.transform.gameObject.name == "Cube")
+                { 
+                Destroy(hitInfo.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+
+    private void RemoveCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        
+        //Destroy(clickPoint);
+    }
+}
